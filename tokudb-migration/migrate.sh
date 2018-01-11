582bcb2ee94bed38289eb0bee45428100bbc132a
Jorgen Phillips <jorgenphillips@gmail.com>
2018-01-10 21:10:13 -0600

    Update migrate.sh

--- tokudb-migration/migrate.sh
+++ tokudb-migration/migrate.sh
@@ -9,7 +9,7 @@ echo "Generating alter_tables.sql"
 echo "" > alter_tables.sql
 
 for DB in $DBS do
-        TABLES=`echo "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = '$DB' AND engine = 'InnoDB'" | mysql -h $DB_host -u $DB_user -p'$DB_pass' $DB | grep -v Tables_in | grep -v TABLE_NAME`
+        TABLES=`echo "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = '$DB' AND engine = 'InnoDB'" | mysql -h $DB_host -u $DB_user -p$DB_pass $DB | grep -v Tables_in | grep -v TABLE_NAME`
         for TABLE in ${TABLES[@]}; do
                 echo "ALTER TABLE $DB.$TABLE ENGINE=TokuDB;" >> alter_tables.sql
         done
